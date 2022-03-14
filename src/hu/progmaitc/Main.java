481141233d85aa6f87b57efc8b48d00a5697fb00
Nagyvaradi Adam <adam.nagyvaradi@gmail.com>
2022-03-14 12:59:46 +0100

    Initial commit

---
+++ src/hu/progmaitc/Main.java
@@ -0,0 +1,120 @@
+package hu.progmaitc;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Kérek egy számot!");
+        int szam1 = scanner.nextInt();
+        System.out.println("Kérek még egy számot!");
+        int szam2 = scanner.nextInt();
+
+       if (szam2 == 0 ){
+           System.out.println(" Nullával nem osztunk!");
+       }else{
+           System.out.println(szam1/szam2);
+        }*/
+
+
+       /* int number =-10;
+        if (number > 0){
+            System.out.println(number-1);;
+        }else{
+            System.out.println(number);
+        }*/
+
+        /*int number = -10;
+        if (number <0){
+            System.out.println("Negativ");
+        }else if (number > 0){
+            System.out.println("pozitiv");
+        }else{
+            System.out.println("nulla");
+        }*/
+
+      /*  String light1 = "Piros";
+        if (light1.equals("zöld")){
+            System.out.println("SZABAD");
+        } else if (light1.equalsIgnoreCase("sárga")){
+            System.out.println("LASSITS!");
+        } else if (light1.equalsIgnoreCase("piros")){
+            System.out.println("MEGÁLLJ!");
+        }else{
+            System.out.println("NEM TUDOM");
+        }*/
+
+
+       /*String month = "december";
+        if (month.equalsIgnoreCase("december")
+                ||month.equalsIgnoreCase("január")
+                || month.equalsIgnoreCase("február")) {
+            System.out.println("TÉL");
+        } else if (month.equalsIgnoreCase("március")
+                ||month.equalsIgnoreCase("április")
+                || month.equalsIgnoreCase("május")) {
+            System.out.println("TAVASZ");
+        } else if (month.equalsIgnoreCase("junius")
+                ||month.equalsIgnoreCase("julius")
+                || month.equalsIgnoreCase("augusztus")) {
+            System.out.println("NYÁR");
+        }else if (month.equalsIgnoreCase("szeptember")
+                ||month.equalsIgnoreCase("október")
+                || month.equalsIgnoreCase("november")) {
+            System.out.println("ŐSZ");
+        } else {
+            System.out.println("nem meghatározható");
+        }*/
+
+        /*int n = 10;
+        for (int i = 0; i < n; i++) {
+            System.out.println(i + 1);
+        }*/
+
+        /*int n = 10;
+        for (int i = 0; i <= n; i++) {
+            System.out.println(n - i);
+        }*/
+
+        /*int n = 10;
+        int result = 1;
+
+        for (int i = 1; i <= n; i++){
+            result *= i;
+        }
+        System.out.println(result);*/
+
+        /*char start = 'a';
+        int until = 26;
+
+        for (int i = 0; i < until; i++){
+            System.out.print(start);
+            start = (char) (start + 1);
+        }*/
+
+        /*int[] numbers = {1, 2, 3};
+        String begin = "[";
+        String end = "]";
+        String answer = "";
+
+
+         System.out.print(begin);
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == numbers.length - 1){
+                answer = "" + numbers[i];
+                System.out.print(answer);
+            } else {
+                answer = numbers[i] + ", ";
+                System.out.print(answer);
+            }
+        }
+
+        System.out.print(end);*/
+
+
+
+    }
+}
